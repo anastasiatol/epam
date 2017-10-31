fe38df26e4e2ea7500938796618a2d622c1858d3
anastasiatol <tolstykoanastasia@gmail.com>
2017-11-01 01:01:28 +0300

    js/task7 fixed (async)

--- js/task7(http-nodejs)/withPromices/withPromises.js
+++ js/task7(http-nodejs)/withPromices/withPromises.js
@@ -1,9 +1,10 @@
 var XMLHttpRequest = require('XMLHttpRequest').XMLHttpRequest;
 
-var resorces = ['http://www.nbrb.by/API/ExRates/Currencies', 'http://www.nbrb.by/API/ExRates/Currencies/141', 'http://www.nbrb.by/API/ExRates/Rates/RUB?onDate=Sun%2C+29+Oct+2017+21%3A00%3A00+GMT&ParamMode=2', 'http://www.nbrb.by/API/ExRates/Rates?onDate=Sun%2C+29+Oct+2017+21%3A00%3A00+GMT&Periodicity=0', 'http://www.nbrb.by/API/ExRates/Rates?onDate=Sun%2C+29+Oct+2017+21%3A00%3A00+GMT&Periodicity=1'];
+var resources = ['http://www.nbrb.by/API/ExRates/Currencies', 'http://www.nbrb.by/API/ExRates/Currencies/141', 'http://www.nbrb.by/API/ExRates/Rates/RUB?onDate=Sun%2C+29+Oct+2017+21%3A00%3A00+GMT&ParamMode=2', 'http://www.nbrb.by/API/ExRates/Rates?onDate=Sun%2C+29+Oct+2017+21%3A00%3A00+GMT&Periodicity=0', 'http://www.nbrb.by/API/ExRates/Rates/USD?onDate=Mon%2C+30+Oct+2017+21%3A00%3A00+GMT&ParamMode=2'];
 
-var getInformationFromServer = function (resorce) {
+var getInformationFromServer = function (resource) {
     return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
             if (xhr.readyState !== 4) return;
             if (xhr.status !== 200) {
@@ -12,16 +13,33 @@ var getInformationFromServer = function (resorce) {
                 resolve(xhr.responseText);
             };
         };
-        xhr.open('GET', resorces[indexResorces], false);
+        xhr.open('GET', resource, true);
         xhr.send();
     });
 };
 
-for (var indexResorces = 0; indexResorces < resorces.length; indexResorces++) {
-    var xhr = new XMLHttpRequest();
-    var data = getInformationFromServer(resorces[indexResorces]);
-    data.then((data) => {
-        console.log(data);
-        console.log('---------------------------------')
-    });
-};
+getInformationFromServer(resources[0])
+    .then(information => {
+        console.log(information);
+        console.log('1---------------------------------');
+        return getInformationFromServer(resources[1]);
+    })
+    .then(information => {
+        console.log(information);
+        console.log('2---------------------------------');
+        return getInformationFromServer(resources[2]);
+    })
+    .then(information => {
+        console.log(information);
+        console.log('3---------------------------------');
+        return getInformationFromServer(resources[3]);
+    })
+    .then(information => {
+        console.log(information);
+        console.log('4---------------------------------');
+        return getInformationFromServer(resources[4]);
+    })
+    .then(information => {
+        console.log(information);
+        console.log('5---------------------------------');
+    })
--- js/task7(http-nodejs)/withoutPromices/withoutPromises.js
+++ js/task7(http-nodejs)/withoutPromices/withoutPromises.js
@@ -1,19 +1,38 @@
 var XMLHttpRequest = require('XMLHttpRequest').XMLHttpRequest;
 
-var resorces = ['http://www.nbrb.by/API/ExRates/Currencies', 'http://www.nbrb.by/API/ExRates/Currencies/141', 'http://www.nbrb.by/API/ExRates/Rates/RUB?onDate=Sun%2C+29+Oct+2017+21%3A00%3A00+GMT&ParamMode=2', 'http://www.nbrb.by/API/ExRates/Rates?onDate=Sun%2C+29+Oct+2017+21%3A00%3A00+GMT&Periodicity=0', 'http://www.nbrb.by/API/ExRates/Rates?onDate=Sun%2C+29+Oct+2017+21%3A00%3A00+GMT&Periodicity=1'];
+var resources = ['http://www.nbrb.by/API/ExRates/Currencies', 'http://www.nbrb.by/API/ExRates/Currencies/141', 'http://www.nbrb.by/API/ExRates/Rates/RUB?onDate=Sun%2C+29+Oct+2017+21%3A00%3A00+GMT&ParamMode=2', 'http://www.nbrb.by/API/ExRates/Rates?onDate=Sun%2C+29+Oct+2017+21%3A00%3A00+GMT&Periodicity=0', 'http://www.nbrb.by/API/ExRates/Rates?onDate=Sun%2C+29+Oct+2017+21%3A00%3A00+GMT&Periodicity=1'];
 
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState !== 4) return;
-    if (xhr.status !== 200) {
-        console.log(xhr.statusText);
-    } else {
-        console.log(xhr.responseText);
-        console.log('---------------------------------------');
+function getInformationFromServer(resource, callback) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== 4) return;
+        if (xhr.status !== 200) {
+            console.log(xhr.statusText);
+            callback();
+        } else {
+            console.log(xhr.responseText);
+            callback();
+        };
     };
+
+    xhr.open('GET', resource, true);
+    xhr.send();
+
 };
 
-for (var indexResorces = 0; indexResorces < resorces.length; indexResorces++) {
-    xhr.open('GET', resorces[indexResorces], false);
-    xhr.send();
-};
+getInformationFromServer(resources[0], function () {
+    console.log('1---------------------------------------');
+    getInformationFromServer(resources[1], function () {
+        console.log('2---------------------------------------');
+        getInformationFromServer(resources[2], function () {
+            console.log('3---------------------------------------');
+            getInformationFromServer(resources[3], function () {
+                console.log('4---------------------------------------');
+                getInformationFromServer(resources[4], function () {
+                    console.log('5---------------------------------------');
+                })
+            })
+        })
+    })
+})
