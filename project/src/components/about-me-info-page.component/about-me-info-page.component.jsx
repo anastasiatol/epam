30644ae340754622d76ba0ba5059e3c40cc29f47
anastasiatol <tolstykoanastasia@gmail.com>
2017-12-02 23:05:57 +0300

    add carousel

--- project/src/components/about-me-info-page.component/about-me-info-page.component.jsx
+++ project/src/components/about-me-info-page.component/about-me-info-page.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Carousel } from './../carousel.component/carousel.component.jsx'
 
 import './about-me-info-page.component.less'
     
@@ -6,19 +7,25 @@ export class AboutMeInfoPage extends Component {
 
     render () {
         return (
-            <div className = 'ak-my-info-page'>
-                <div className = 'ak-my-info-page_title'>
-                    I've built this website
-                </div>
-                <div className = 'ak-my-info-page_text'>
+            <div className = 'ak-container_my-info-page ak-my-info-page'>
+                <div className = 'ak-my-info-page_text-photo'>
+                    <div className = 'ak-my-info-page_title'>
+                        <div className = 'ak-my-info-page_title'>
+                            I've built this website
+                        </div>
+                        <div className = 'ak-my-info-page_text'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ullamcorper a mauris vel pharetra. Fusce mollis interdum porttitor. Curabitur vel erat aliquet, efficitur urna a, blandit ipsum. Aenean dapibus, enim sit amet pharetra congue, diam erat bibendum eros, a varius massa mi et tortor. Mauris consectetur mi a turpis pretium, eget tincidunt ligula tristique. In hendrerit nulla libero, id tristique tortor placerat eget. Suspendisse potenti.
+                            Ut feugiat nibh eros, a laoreet velit gravida non. Aliquam erat volutpat. Proin id efficitur erat. Vivamus tempus pharetra dolor laoreet euismod. Sed sed lacus quis ex blandit rutrum. Maecenas condimentum dolor vel diam ornare, vitae suscipit nulla euismod. Proin ac augue eget nulla hendrerit mollis. Aliquam vehicula urna a fermentum sodales. Duis ut sollicitudin ligula.
+                            Duis a nunc at risus ornare consequat a vitae metus. Aenean purus lacus, convallis eleifend bibendum eu, blandit quis mauris. Nunc vestibulum turpis in dui consequat, non efficitur eros pulvinar. Donec mattis ultrices libero vitae viverra. Pellentesque sed velit consequat leo suscipit interdum. Vestibulum imperdiet aliquam lacus, a sollicitudin nibh lacinia sit amet. Phasellus in eros et sem congue lacinia. Fusce non augue vitae velit suscipit malesuada. Aenean molestie accumsan erat a volutpat.
+                            Sed id augue vestibulum, dignissim massa pellentesque, pellentesque eros. Curabitur laoreet enim vitae mauris rutrum feugiat vel consectetur ligula. Mauris commodo ac velit eget sagittis. Praesent maximus imperdiet libero, non vehicula dui scelerisque in. Fusce aliquet blandit elit, sit amet euismod orci porta sit amet. Integer elementum nulla nec nulla porttitor, eu dignissim arcu sollicitudin. Aliquam nisi augue, dignissim in arcu vel, tincidunt venenatis dui. Duis augue magna, dictum a nibh ut, tincidunt vehicula ante. Suspendisse nec elit elementum, semper dui a, ullamcorper risus. Cras porta cursus sem sit amet egestas.
+                            Morbi ac viverra leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at lacus venenatis, placerat erat eget, rutrum purus. In congue congue fringilla. Praesent vel hendrerit nibh. Morbi id dolor eget mi consequat condimentum. Ut vulputate, sapien sit amet pharetra accumsan, orci lectus fermentum ligula, id hendrerit ante purus ac metus. Nullam sem sem, iaculis malesuada lacus id, ullamcorper viverra metus. Morbi in ultrices magna, sed fringilla augue. Donec id est justo. Quisque laoreet rhoncus mi a maximus. Aliquam eget metus eu ex porta posuere. Donec orci eros, efficitur vulputate tellus ut, pellentesque dapibus erat. Nulla id maximus felis. Nam nec aliquet neque.
+                        </div>
+                    </div>
+
+                    <img src='./src/img/IMG_5917.JPG' className = 'ak-my-info-page_photo'/>
 
                 </div>
-                <div className = 'ak-my-info-page_photo'>
-                
-                </div>
-                <div className = 'ak-my-info-page_carousel'>  
-
-                </div>
+                <Carousel/> 
             </div>
         )
     }
---
+++ project/src/components/carousel.component/carousel.component.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import "./carousel.component.less";
+
+export class Carousel extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentImg :7
+        }
+        this.imgArr = [
+            './../src/img/About.png',
+            './../src/img/Add movie.png',
+            './../src/img/Add movie(validations).png',
+            './../src/img/Added notifications.png',
+            './../src/img/Empty library.png',
+            './../src/img/Main Page (Hide Dashboard).png',
+            './../src/img/Main Page (Search).png',
+            './../src/img/Main Page(add to library).png',
+            './../src/img/Main Page(remove from library).png',
+            './../src/img/Main Page(Scroll).png',
+            './../src/img/Main Page(Scroll).png',
+            './../src/img/Movie Information.png',
+            './../src/img/My Library.png',
+            './../src/img/Remove movie from library.png',
+            './../src/img/Support.png'
+        ]
+    }
+
+    previousImg() {
+        if (this.state.currentImg>0) {
+            this.setState({currentImg:(this.state.currentImg-1)})
+        } else {
+            this.setState({currentImg: this.imgArr.length})
+        }
+    }
+    nextImg() {
+        if (this.state.currentImg<(this.imgArr.length-1)) {
+            this.setState({currentImg:(this.state.currentImg+1)})
+        } else {
+            this.setState({currentImg:0})
+        }
+    }
+
+    render() {
+        return (
+            <div className = 'ak-carousel ak-my-info-page_carousel'>
+                <img className = 'ak-carousel_picture'
+                    src = {this.imgArr[this.state.currentImg]}  
+                    alt = 'pic'/> 
+                <div className = 'ak-carousel_buttons'>
+                    <div className = 'ak-carousel_single-button'
+                        onClick = {()=> this.previousImg()}>
+                        prev
+                    </div>
+                    <div className = 'ak-carousel_single-button'
+                        onClick = {()=> this.nextImg()}>
+                        next
+                    </div>
+                </div>   
+            </div>
+        )
+    }
+}
